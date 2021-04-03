22e56267b8ceee6f3c8b5c8dc418747214fd6c2b
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-03 16:17:49 -0700

    comp inventory

--- MEAN_STACK_PROJECT/angular-Invetory-Tasks-CRUD/src/app/app.module.ts
+++ MEAN_STACK_PROJECT/angular-Invetory-Tasks-CRUD/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
