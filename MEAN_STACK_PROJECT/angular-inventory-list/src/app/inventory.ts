ae5c19428d86c71277c7ad3a137ff1918ae96144
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-03 17:41:01 -0700

    inventoryDisplay

---
+++ MEAN_STACK_PROJECT/angular-inventory-list/src/app/inventory.ts
@@ -0,0 +1,4 @@
+export interface inventoryComputer {
+    id: number;
+    name: string;
+  }
--- MEAN_STACK_PROJECT/angular-inventory-list/src/app/inventory/inventory.component.ts
+++ MEAN_STACK_PROJECT/angular-inventory-list/src/app/inventory/inventory.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryComponent implements OnInit {
 
-  inventoryComputer = 'A';
+  inventoryComputer = {
+    id: 1,
+    name:'A'
+};
 
   constructor() { }
 
