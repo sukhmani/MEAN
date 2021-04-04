511e34a61e9525b70bf1e434eb73e0d6a394cc47
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-04 06:09:03 -0700

    express updates

--- MEAN_STACK_PROJECT/backend/app.js
+++ MEAN_STACK_PROJECT/backend/app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 
 
 app.get('/Lists',(req, res) => {
-  Task.find({})
+  List.find({})
         .then(lists => res.send(lists))  
         .catch((error) => console.log(error));
 
@@ -29,6 +29,27 @@ app.post('/', (req, res) =>{
     ( new Task({'title': req.body.title}))
     .save()
     .then((List) => res.send(List))
+    .catch((error) => console.log(error));
 });
+/*
+app.get('/lists/:listId',(req, res) => {
+        List.find({_id: req.params.listId})
+        .then((List) => res.send(List))
+        .catch((error) => console.log(error));
+});
+*/
+app.patch('/lists/:listId', (req, res) => {
+        List.findOneAndUpdate({'_id': req.params.listId
+        },{$set: req.body})
+        .then((List) => res.send(List))
+        .catch((error) => console.log(error));
+});
+
+ app.delete('/lists/:listId', (req, res) => {
+        List.findOneAndDelete(req.params.listId)
+        .then((list) => res.send(list))
+        .catch((error) => console.log(error));
+})
+
 
 app.listen(3000, () => console.log("Server connected on port 3000"));
