4424a653aa421caf644e1201686682d6e8d02ee9
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-05 14:06:57 -0700

    cross origin - cors

--- MEAN_STACK_PROJECT/backend/app.js
+++ MEAN_STACK_PROJECT/backend/app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('./database/mongoose');
 
+var cors = require('express-cors')
+ 
+app.use(cors({
+    allowedOrigins: [
+        'github.com', 'google.com'
+    ]
+}))
 
 app.use(express.json());
 app.listen(3000,() => console.log("Server connected on port 3000"));
