d7aeb64819fcd60822f08612fac66e86dc6e5743
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-07 15:45:20 -0700

    update button

--- MEAN_STACK_PROJECT/frontend-angular-computers/src/app/update/update.component.ts
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/update/update.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { update } from './update';
 
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
   styleUrls: ['./update.component.css']
 })
-export class UpdateComponent implements OnInit {
+export class UpdateComponent  {
 
-  constructor() { }
+  type = ['A', 'B',
+  'C', 'D'];
 
-  ngOnInit(): void {
-  }
+  Version = ['1', '2',
+  '3', '4'];
 
+  Memory = ['1', '2',
+  '3', '4'];
+
+mod = new update(9, 'new computer', this.type[0], 'lkj','');
+
+submitted = false;
+
+onSubmit() { this.submitted = true; }
+
+
+get diagnostic() { return JSON.stringify(this.mod); }
+
+update() {
+  this.mod = new update(42, '', '','','');
+}
 }
---
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/update/update.ts
@@ -0,0 +1,12 @@
+export class update {
+
+    constructor(
+      public id: number,
+      public name: string,
+      public type: string,
+      public version: string,
+      public memory: string
+      //public version?: string
+    ) {  }
+  
+  }
