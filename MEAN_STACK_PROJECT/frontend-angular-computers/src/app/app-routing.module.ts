55ed6e39630ff461ef97502de964b7d714853b92
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-07 01:26:22 -0700

    crud routes work

--- MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app-routing.module.ts
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ComputersComponent } from './computers/computers.component';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
 
 const routes: Routes = [
-  { path: 'computers', component: ComputersComponent }
+  { path: 'computers', component: ComputersComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'read', component: ReadComponent },
+  { path: 'update', component: UpdateComponent },
+  { path: 'delete', component: DeleteComponent }
 ];
 
 @NgModule({
