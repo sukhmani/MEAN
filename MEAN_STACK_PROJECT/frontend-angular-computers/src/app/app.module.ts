584b943ab0a9d833bd6bb434c4465d300efdd7d6
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-07 00:52:34 -0700

    component crud

--- MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app.module.ts
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComputersComponent } from './computers/computers.component';
 
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { FormsModule } from '@angular/forms';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component'; // <-- NgModel lives here
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ComputersComponent,
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent,
+    DeleteComponent,
     
   ],
   imports: [
