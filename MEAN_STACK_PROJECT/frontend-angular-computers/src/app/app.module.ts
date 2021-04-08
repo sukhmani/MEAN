ec424dc645b9babbef561a58a7a258cd9c69ecd1
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-07 19:22:35 -0700

    server comm

--- MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app.module.ts
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ import { DeleteComponent } from './delete/delete.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
