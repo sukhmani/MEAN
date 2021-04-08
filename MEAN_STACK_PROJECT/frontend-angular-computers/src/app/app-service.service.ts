8c93df7a177d80c2d08e7e0199141408495456e0
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-07 22:08:55 -0700

    connection to express

--- MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app-service.service.ts
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app-service.service.ts
@@ -7,4 +7,8 @@ import { HttpClient } from '@angular/common/http';
 export class AppServiceService {
 
   constructor(private http: HttpClient) { }
+
+  getData(){
+    return this.http.get('api/getData')
+  }
 }
--- MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app.component.ts
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppServiceService } from './app-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Computer Inventory ';
+
+  constructor(private service : AppServiceService){
+
+  }
+
+  ngOnInit(){
+    this.getDataFromAPI();
+  }
+
+getDataFromAPI(){
+  this.service.getData().subscribe((response) => {
+    console.log('Response is:',response)
+
+  }, (error) => {
+    console.log('error is',error);
+    
+  })
+  
+  
+}
 }
