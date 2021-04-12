b7f4a5a9a8ebf49d365a95df524406d151a925b8
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-12 02:45:11 -0700

    api

--- MEAN_STACK_PROJECT/backend/app.js
+++ MEAN_STACK_PROJECT/backend/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const mongoose = require('./database/mongoose');
 const computer = require('./database/models/computer');
