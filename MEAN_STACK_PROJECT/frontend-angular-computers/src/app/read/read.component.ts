c422d18b18f54752adf6c8ae2da31c551c742fd4
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-17 16:19:02 -0700

    read

--- MEAN_STACK_PROJECT/frontend-angular-computers/src/app/read/read.component.ts
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/read/read.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Read } from './read';
+
 
 @Component({
   selector: 'app-read',
@@ -7,13 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadComponent implements OnInit {
 
-  constructor() { }
+  type = ['A', 'B',
+  'C', 'D'];
+
+mod = new Read(this.type[0]);
+
+  constructor(private httpClient: HttpClient ) {
+    
+   }
 
   ngOnInit(): void {
   }
 
-  Read(){
-    alert('read');
-  }
-
+  
+  Read() {
+  
+    this.httpClient.get('http://localhost:3000/read-button')
+    .subscribe( (response: any) => {
+      console.log(response);
+    });
+}
 }
---
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/read/read.ts
@@ -0,0 +1,12 @@
+export class Read {
+
+    constructor(
+     // public id: number,
+     // public name: string,
+      public type: string,
+     // public version: string,
+    //  public memory: string
+      //public version?: string
+    ) {  }
+  
+  }
