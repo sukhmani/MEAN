535d14884681bedc1ddfc8edf7795c34de67c6b1
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-16 20:06:24 -0700

    read redesigned

--- MEAN_STACK_PROJECT/frontend-angular-computers/src/app/read/read.component.ts
+++ MEAN_STACK_PROJECT/frontend-angular-computers/src/app/read/read.component.ts
@@ -1,44 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { create } from '../create/create';
-import { HttpClientModule } from '@angular/common/http';
-import { FormGroup, FormControl, FormBuilder} from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
   styleUrls: ['./read.component.css']
 })
+export class ReadComponent implements OnInit {
 
+  constructor() { }
 
-export class ReadComponent implements OnInit{
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
- 
+
   Read(){
-    alert('Read');
-  }
-
-  }
-  
-
-  /*Read() {
-  
-    , this.mod)
-    .subscribe( response => {
-      console.log(response);
-    });
-  }
-
-  get diagnostic() { return JSON.stringify(this.mod); }
-
-  constructor(private httpClient: HttpClient){}
-
-  ngOnInit(): void {
+    alert('read');
   }
 
 }
-*/
-
-
