81a6676a3a27ba40c810e96581cea3644f9e24cd
sukhmani <sukhmani@bellevuecollege.edu>
2021-04-23 00:02:43 -0700

    mod hm page

--- PROJECT-CRUD/client/src/app/app-routing.module.ts
+++ PROJECT-CRUD/client/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { CreateComponent } from './create/create.component';
 import { ReadComponent } from './read/read.component';
 import { UpdateComponent } from './update/update.component';
 import { DeleteComponent } from './delete/delete.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'computers', component: ComputersComponent },
   { path: 'create', component: CreateComponent },
   { path: 'read', component: ReadComponent },
   { path: 'update', component: UpdateComponent },
-  { path: 'delete', component: DeleteComponent }
+  { path: 'delete', component: DeleteComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
